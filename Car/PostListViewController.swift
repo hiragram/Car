752db749f81e696c4d56c5c4d5af4bd5fe274324
hiragram <y@yura.me>
2017-01-19 02:27:07 +0900

    Add PostListViewController

---
+++ Car/PostListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PostListViewController.swift
+//  Car
+//
+//  Created by yuyahirayama on 2017/01/19.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+import UIKit
+
+final class PostListViewController: UIViewController, StoryboardInstantitable {
+
+  @IBOutlet private weak var tableView: UITableView!
+
+}
