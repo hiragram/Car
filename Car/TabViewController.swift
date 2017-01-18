461ec151e6235ade39c3460c29aaa0602f8b617b
hiragram <y@yura.me>
2017-01-19 02:21:22 +0900

    Add dummy view

--- Car/TabViewController.swift
+++ Car/TabViewController.swift
@@ -8,6 +8,19 @@
 
 import UIKit
 
-final class TabViewController: UIViewController, StoryboardInstantitable {
-  
+final class TabViewController: UITabBarController, StoryboardInstantitable {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let viewControllers = [
+      UIColor.red,
+      .cyan,
+      ].map { color -> UIViewController in
+        let vc = UIViewController.init()
+        vc.view.backgroundColor = color
+        vc.title = "title"
+        return vc
+    }
+
+    setViewControllers(viewControllers, animated: true)
+  }
 }
