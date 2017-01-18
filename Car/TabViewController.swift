c2d0904141004975a85dd7d7d9da309adff7ae11
hiragram <y@yura.me>
2017-01-19 01:42:10 +0900

    Change initial screen to TabViewController

---
+++ Car/TabViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TabViewController.swift
+//  Car
+//
+//  Created by yuyahirayama on 2017/01/19.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+import UIKit
+
+final class TabViewController: UIViewController, StoryboardInstantitable {
+  
+}
