fecdfc86f08062a92ea3789ff8ba07c258b7f181
Yuya <y@yura.me>
2017-02-02 21:58:37 +0900

    Add empty cell

---
+++ Car/PostImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  PostImageCell.swift
+//  Car
+//
+//  Created by yuya_hirayama on 2017/02/02.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+import UIKit
+
+class PostImageCell: UITableViewCell {
+  
+}
--- Car/PostListViewController.swift
+++ Car/PostListViewController.swift
@@ -25,8 +25,7 @@ final class PostListViewController: UIViewController, StoryboardInstantiatable {
 
       tableView.rx.modelSelected(PostListViewModel.Item.self).subscribe(onNext: { [weak self] (item) in
         self?.show(PostViewController.self, sender: nil, configuration: { (vc) in
-          let vm = PostViewModel.init()
-          vm.context.value = .entity(item)
+          let vm = PostViewModel.init(context: .entity(item))
           vc.vm.value = vm
         })
       }).addDisposableTo(bag)
